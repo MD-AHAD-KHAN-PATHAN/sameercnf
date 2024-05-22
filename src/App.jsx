55575e2a7df3818321a83d49f183ac1d08c6619a
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2024-05-22 13:44:52 +0600

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
         </div>
         <div>
           <ul className="flex gap-3">
-            <li><a className="p-4 font-bold" href="home">Home</a></li>
-            <li><a className="p-4 font-bold" href="about">About Us</a></li>
-            <li><a className="p-4 font-bold" href="profile">Profile</a></li>
-            <li><a className="p-4 font-bold" href="contact">Contact</a></li>
+            <li><a className="p-4 font-bold" href="/home">Home</a></li>
+            <li><a className="p-4 font-bold" href="/about">About Us</a></li>
+            <li><a className="p-4 font-bold" href="/profile">Profile</a></li>
+            <li><a className="p-4 font-bold" href="/contact">Contact</a></li>
           </ul>
         </div>
       </div>
